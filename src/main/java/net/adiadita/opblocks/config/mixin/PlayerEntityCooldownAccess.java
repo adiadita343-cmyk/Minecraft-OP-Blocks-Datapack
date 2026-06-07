b3c012a538573fe4dfe959f3031d26e04f7b7f02
adios <adiadita343@gmail.com>
2026-06-07 21:48:04 +0300

    Delete src/main/java/net/adiadita/opblocks/config/mixin/PlayerEntityCooldownAccess.java

--- src/main/java/net/adiadita/opblocks/config/mixin/PlayerEntityCooldownAccess.java
+++
@@ -1,6 +0,0 @@
-package net.adiadita.opblocks.mixin;
-
-public interface PlayerEntityCooldownAccess {
-    long ultimateopblocks$getLastCosmicDropTime();
-    void ultimateopblocks$setLastCosmicDropTime(long time);
-}
