0d99767b5657d0f6c29fce93cca3e714d199d891
adios <adiadita343@gmail.com>
2026-06-07 21:48:54 +0300

    Create PlayerEntityCooldownAccess.java

---
+++ src/main/java/net/adiadita/opblocks/util/PlayerEntityCooldownAccess.java
@@ -0,0 +1,6 @@
+package net.adiadita.opblocks.util;
+
+public interface PlayerEntityCooldownAccess {
+    long ultimateopblocks$getLastCosmicDropTime();
+    void ultimateopblocks$setLastCosmicDropTime(long time);
+}
